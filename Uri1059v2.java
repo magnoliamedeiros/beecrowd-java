d3425dde8768f5d9943de5ec809124f59c957ebe
magnoliamedeiros <magnoliamedeiros@gmail.com>
2022-03-14 18:04:19 -0300

    BEE 1059: Números Pares (v2)

---
+++ Uri1059v2.java
@@ -0,0 +1,13 @@
+public class Uri1059v2 {
+    public static void main (String[] args) {
+
+        // BEE 1059: Números Pares (v2)
+
+        for (int numero = 1; numero <= 100; numero++) {
+
+            if (numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        }
+    }
+}

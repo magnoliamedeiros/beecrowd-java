27e4abc0f709ed20d8011918bde15fe761d9d138
magnoliamedeiros <magnoliamedeiros@gmail.com>
2022-03-15 08:54:50 -0300

    BEE: Par ou Impar

---
+++ Uri1074.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class Uri1074 {
+    public static void main (String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        // BEE: Par ou Impar
+
+        // declaracao de variaveis
+        int valor;
+        int repeticoes;
+
+        // entrada
+        // numeros me dá a quantida de repeticoes
+        repeticoes = teclado.nextInt();
+
+        // processamento e saida
+        for (int cont = 0; cont < repeticoes; cont++) {
+
+            // entrada dos valores
+            valor = teclado.nextInt();
+
+            // vamos testar
+            if (valor == 00) {
+                System.out.println("NULL");
+            }
+            else if (valor % 2 == 0 && valor > 0) {
+                System.out.println("EVEN POSITIVE");
+            }
+            else if (valor % 2 == 0 && valor < 0) {
+                System.out.println("EVEN NEGATIVE");
+            }
+            else if (valor % 2 != 0 && valor > 0) {
+                System.out.println("ODD POSITIVE");
+            }
+            else if (valor % 2 !=0 && valor < 0) {
+                System.out.println("ODD NEGATIVE");
+            }
+        }
+
+        teclado.close();
+    }
+}

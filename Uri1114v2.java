d9edfdf590c5dd9aabbef3fe841cb9c956dbac68
magnoliamedeiros <magnoliamedeiros@gmail.com>
2022-03-15 21:56:17 -0300

    BEE 1114: Senha Fixa (v2)

--- Uri1114v2.java
+++ Uri1114v2.java
@@ -13,11 +13,13 @@ public class Uri1114v2 {
             // entrada
             senha = teclado.nextInt();
 
-            // processamento e saida
+            // processamento e saida 1
             if (senha != 2002) {
                 System.out.println("Senha Invalida");
             }
         } while (senha != 2002);
+
+        // saida 2
         System.out.println("Acesso Permitido");
 
         teclado.close();

82d6951904b57640074e04034ba6d87343785783
magnoliamedeiros <magnoliamedeiros@gmail.com>
2022-03-15 21:52:25 -0300

    BEE 1038: Lanche (v2)

--- Uri1038v2.java
+++ Uri1038v2.java
@@ -4,9 +4,9 @@ public class Uri1038v2 {
     public static void main (String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        // BEE 1038: Lanche (Versão 2)
+        // BEE 1038: Lanche (v2)
 
-        // declaração de variáveis
+        // declaracao de variaveis
         int codigo, quantidade;
         double total;
 
@@ -15,20 +15,20 @@ public class Uri1038v2 {
         quantidade = teclado.nextInt();
 
         // processamento
-        if (codigo == 1) {          // o código é 1?
+        if (codigo == 1) {          // o codigo eh 1?
             total = quantidade * 4.0;
         }
-        else if (codigo == 2) {     // o código é 2?
+        else if (codigo == 2) {     // o codigo eh 2?
             total = quantidade * 4.50;
-        } else if (codigo == 3) {   // o código é 3?
+        } else if (codigo == 3) {   // o codigo eh 3?
             total = quantidade * 5.0;
-        } else if (codigo == 4) {   // o código é 4?
+        } else if (codigo == 4) {   // o codigo eh 4?
             total = quantidade * 2.0;
-        } else {                    // o código NÃO é 4, só pode ser 5
+        } else {                    // o codigo NAO eh 4, soh pode ser 5
             total = quantidade * 1.50;
         }
 
-        // saída
+        // saida
         System.out.printf("Total: R$ %.2f\n", total);
 
         teclado.close();

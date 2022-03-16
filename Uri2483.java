1337ce903320734d0b9ce884e8152c42ed388206
magnoliamedeiros <magnoliamedeiros@gmail.com>
2022-03-16 10:02:21 -0300

    BEE 2483: Feliz Nataaal!

--- Uri2483.java
+++ Uri2483.java
@@ -1,17 +1,27 @@
 import java.util.Scanner;
 
 public class Uri2483 {
-
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
+        Scanner teclado = new Scanner(System.in);
 
-        // beecrowd | 2483
-
-        // ENTRADA
-        int numero = entrada.nextInt();
-
-        // PROCESSAMENTO
+        // BEE 2483: Feliz Nataaal!
         
+        // declaracao de variaveis
+        int i;
 
+        // entrada
+        i = teclado.nextInt();
+
+        // processamento
+        System.out.print("Feliz nat");
+
+        for (int cont = 0; cont < i; cont++) {
+            System.out.print("a");
+        }
+
+        // saida
+        System.out.println("l!");
+        
+        teclado.close();
     }
 }

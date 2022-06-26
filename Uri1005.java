cf8e85a618d4e3b56e7cacb5c41e2df9684d3ea6
Magnólia Medeiros <magnoliamedeiros@gmail.com>
2022-06-26 18:07:47 -0300

    BEE 1005: Media 1

--- Uri1005.java
+++ Uri1005.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 public class Uri1005{
 
-    public static void Uri1005 (String args[]){
+    public static void main(String args[]){
         Scanner teclado = new Scanner(System.in);
+
+        // BEE 1005: Média 1
         
         // declaração
         double a, b, media;
@@ -15,7 +17,7 @@ public class Uri1005{
         // processamento
         media = (a * 3.5 + b * 7.5) / 11;
     
-        // saída com 5 digitos de precisao
+        // saída com 5 digitos de precisão
         System.out.printf("MEDIA = %.5f\n", media);
 
         teclado.close();

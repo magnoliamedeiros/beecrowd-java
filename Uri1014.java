100754e5c8e0dc78a53ab51acc9524ce6c4d9754
Magnólia Medeiros <magnoliamedeiros@gmail.com>
2022-06-27 10:32:50 -0300

    BEE 1014: Consumo

--- Uri1014.java
+++ Uri1014.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Uri1014 {
 
-    public static void main (String args[]) {
+    public static void main(String args[]) {
         Scanner teclado = new Scanner(System.in);
 
-        // Beecrowd 1014 = Consumo
+        // BEE 1014 = Consumo
 
-        // Declaração de variáveis
+        // declaração
         int distanciaTotalPercorrida;
         double combustivelGasto;
         double consumoMedio;

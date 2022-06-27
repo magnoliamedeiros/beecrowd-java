8bac0601502fc422ad780daeb1f242fb7314d73d
Magnólia Medeiros <magnoliamedeiros@gmail.com>
2022-06-27 10:35:17 -0300

    BEE 1015: Distância entre dois pontos (incompleto)

--- Uri1015.java
+++ Uri1015.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class Uri1015 {
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
 
-        // BEE 1015 = Distancia Entre Dois Pontos
+        // BEE 1015: Distancia Entre Dois Pontos
         
-        // declaracao de variaveis
+        // declaração
         double x1, y1;
         double x2, y2;
 
@@ -15,7 +15,7 @@ public class Uri1015 {
 
         // processamento
 
-        // saida
+        // saída
         
         teclado.close();         
     }

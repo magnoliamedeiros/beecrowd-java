1d5a163788caa34a05dcfb4e667678804e3cf076
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2023-04-02 19:52:31 -0300

    // BEE 1013: O maior

--- Uri1013.java
+++ Uri1013.java
@@ -6,9 +6,9 @@ public class Uri1013 {
     public static void main(String args[]) {
         Scanner teclado = new Scanner(System.in);
 
-        // BEE 1013: O Maior
+        // BEE 1013: O maior
 
-        // declaração
+        // declaracao de variaveis
         int a, b, c, maiorAB, max;
 
         // entrada
@@ -20,7 +20,7 @@ public class Uri1013 {
         maiorAB = (a + b + Math.abs(a - b)) / 2;
         max = (c + maiorAB + Math.abs(maiorAB - c)) / 2;
 
-        // saída
+        // saida
         System.out.println(max + " eh o maior");
 
         teclado.close();

95d9e039f20d264c9800ea4dcc1741a3e83797e1
Magnolia Medeiros <magnoliamedeiros@gmail.com>
2023-04-02 19:53:43 -0300

    // BEE 1014: Consumo

--- Uri1014.java
+++ Uri1014.java
@@ -5,9 +5,9 @@ public class Uri1014 {
     public static void main(String args[]) {
         Scanner teclado = new Scanner(System.in);
 
-        // BEE 1014 = Consumo
+        // BEE 1014: Consumo
 
-        // declaração
+        // declaracao de variaveis
         int distanciaTotalPercorrida;
         double combustivelGasto;
         double consumoMedio;
@@ -19,7 +19,7 @@ public class Uri1014 {
         // processamento
         consumoMedio = (float)distanciaTotalPercorrida/combustivelGasto;
 
-        // saída
+        // saida
         System.out.printf("%.3f km/l\n", consumoMedio);
 
         teclado.close(); 
